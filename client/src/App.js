dd840e6eb6e6bad9d30a0e64a85cd4835d05647d
asim <asimjaipuri99@gmail.com>
2020-12-04 23:07:49 +0530

    finish routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import MenuBar from './components/MenuBar';
+import { Container } from 'semantic-ui-react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Container>
+        <MenuBar />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/register' component={Register} />
+        </Switch>
+      </Container>
+    </Router>
   );
 }
 
---
+++ client/src/components/MenuBar.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { Menu } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
+const MenuBar = () => {
+  const pathname = window.location.pathname;
+  const path = pathname === '/' ? 'home' : pathname.substr(1);
+  const [state, setstate] = useState(path);
+
+  const handleItemClick = (e, { name }) => setstate(name);
+
+  return (
+    <div>
+      <Menu pointing secondary size='massive' color='teal'>
+        <Menu.Item
+          name='home'
+          active={state === 'home'}
+          onClick={handleItemClick}
+          as={Link}
+          to='/'
+        />
+
+        <Menu.Menu position='right'>
+          <Menu.Item
+            name='login'
+            active={state === 'login'}
+            onClick={handleItemClick}
+            as={Link}
+            to='/login'
+          />
+          <Menu.Item
+            name='register'
+            active={state === 'register'}
+            onClick={handleItemClick}
+            as={Link}
+            to='/register'
+          />
+        </Menu.Menu>
+      </Menu>
+    </div>
+  );
+};
+
+export default MenuBar;
